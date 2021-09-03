b2f65e030128f0f70f9e2830fd2a7e5c8db0a2e8
Tanmay Srivastava <ktsrivastava29@gmail.com>
2021-09-04 01:00:48 +0530

    Responsive

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-// ---------horizontal-navbar-menu-----------
-var tabsNewAnim = $('#navbar-animmenu');
-var selectorNewAnim = $('#navbar-animmenu').find('li').length;
-//var selectorNewAnim = $(".tabs").find(".selector");
-var activeItemNewAnim = tabsNewAnim.find('.active');
-var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
-var itemPosNewAnimLeft = activeItemNewAnim.position();
-$(".hori-selector").css({
-    "left":itemPosNewAnimLeft.left + "px",
-    "width": activeWidthNewAnimWidth + "px"
-});
-$("#navbar-animmenu").on("click","li",function(e){
-    $('#navbar-animmenu ul li').removeClass("active");
-    $(this).addClass('active');
-    var activeWidthNewAnimWidth = $(this).innerWidth();
-    var itemPosNewAnimLeft = $(this).position();
-    $(".hori-selector").css({
-        "left":itemPosNewAnimLeft.left + "px",
-        "width": activeWidthNewAnimWidth + "px"
-    });
-});
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
